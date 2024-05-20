a6fd69a1a9fc5fb7a18f35b38ab72eccaa3d4527
Elminson De Oleo Baez <edeoleo@gmail.com>
2024-05-20 18:56:40 -0400

    Refactor file filtering logic for directories

--- checkdir/util.py
+++ checkdir/util.py
@@ -8,7 +8,10 @@ def list_files(path, file_filter='*'):
     data = os.listdir(path)
     result = {}
     for item in data:
-        if fnmatch.fnmatch(item, file_filter):
+        if os.path.isdir(os.path.join(path, item)):
+            print("Directory: " + item)
+            result[item] = list_files(os.path.join(path, item), file_filter)
+        elif fnmatch.fnmatch(item, file_filter):
             print("Match: " + item)
             result[item] = os.path.isdir(os.path.join(path, item))
     return result
